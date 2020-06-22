10fadf7ce8870a89af0e1f671d611f0c64415b4f
Cassie Cecott <casscecott@gmail.com>
2020-06-22 11:00:00 -0400

    click even on nav, mild styles

--- project/src/app/header/header.component.ts
+++ project/src/app/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { SiteService } from '../site.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
 export class HeaderComponent implements OnInit {
+
+
+  hidden: boolean = true;
 
   constructor(private service: SiteService) { }
 
@@ -15,6 +21,14 @@ export class HeaderComponent implements OnInit {
 
   getDefaultTrails() {
     this.service.getDefaultTrails();
+  }
+
+  toggleClass() {
+    if (this.hidden) {
+      this.hidden = false;
+    } else {
+      this.hidden = true;
+    }
   }
 
 }
